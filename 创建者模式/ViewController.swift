708d3a83cb0d73e0acc5e71a00fbe0d64666e6c4
MH <729263080@qq.com>
2023-05-05 14:49:43 +0800

    Initial Commit

---
+++ 创建者模式/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  创建者模式
+//
+//  Created by menghao on 2023/5/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
